cfc86f5ac857e727d36de2ac59d898b7975339d7
kunalsanwadkar01 <kunalsanwadkar@gmail.com>
2022-06-12 16:04:59 +0530

    hosts

--- citymall/settings.py
+++ citymall/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-z&zri+e3p!a5-j3mux@mmg79v-82x!!$@e*%n-ddchyrp=fo*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'projectcitymall.herokuapp.com']
 
 
 # Application definition
