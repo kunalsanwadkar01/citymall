2acab22f1ed0223c1ad7b8e11a5f60a0b55c14f3
kunalsanwadkar01 <kunalsanwadkar@gmail.com>
2022-06-14 17:24:01 +0530

    update urls

--- citymall/views.py
+++ citymall/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import JsonResponse
 
+
 def landingpage(request):
 
-    return render(request,'landingpage.html')
+    return render(request, 'landingpage.html')
